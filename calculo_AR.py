b9ca26fd465974a75009db068f18361a9fc8cc44
Giovanni Marçon Rossi <giovanni.mar.rossi@gmail.com>
2019-11-19 15:28:54 -0300

    Cauculo não ordena mais os arquivos de entrada antes de calcular o AR

--- calculo_AR.py
+++ calculo_AR.py
@@ -1,43 +1,18 @@
 from sklearn.metrics.cluster import adjusted_rand_score
 import re
 
-class ponto:
-    nome = ''
-    cluster = -1
+arq_real = open('datasets/c2ds1-2spReal.clu', 'r')
+arq = open('c2ds1-2sp.txt-k-means-k5.clu', 'r')
 
-    def __init__(self, nom, clu):   #construtor
-        self.nome = nom
-        self.cluster = clu
-    
-    def getNome(self):
-        return self.nome
-
-    def getCluster(self):
-        return self.cluster
-
-arq_real = open('datasets/monkeyReal1.clu', 'r')
-arq_teste = open('datasets/monkey.txt-k-means-k12.clu', 'r')
-
-pontos_real = []
-pontos_real_sort = []
-pontos = []
-pontos_sort = []
+particao_real = []
+particao = []
 
 for line in arq_real:
     param = re.split('[ ,\n,\t]', line)
-    pontos_real_sort.append(ponto(param[0], int(param[1])))
+    particao_real.append(int(param[1]))
 
-for line in arq_teste:
+for line in arq:
     param = re.split('[ ,\n,\t]', line)
-    pontos_sort.append(ponto(param[0], int(param[1])))
+    particao.append(int(param[1]))
 
-pontos_real_sort = sorted(pontos_real_sort, key = ponto.getNome)
-pontos_sort = sorted(pontos_sort, key = ponto.getNome)
-
-for i in range(0, len(pontos_real_sort)):
-    pontos_real.append(pontos_real_sort[i].getCluster())
-
-for i in range(0, len(pontos_sort)):
-    pontos.append(pontos_sort[i].getCluster())
-
-print(adjusted_rand_score(pontos_real, pontos))
+print(adjusted_rand_score(particao_real, particao))
